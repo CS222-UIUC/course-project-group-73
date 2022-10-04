5e23b5be655798c70525fa30d31cb5fc7d945c01
David Sohn <dhsohn2@illinois.edu>
2022-10-04 16:29:15 -0500

    added distance method

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ import random
 import operator
 import math
 # import numpy as np
-
-
-
+def distance(song1, song2, k):
+    distance_ = 0
+    song1_feature_1 = song1[0]
+    song1_feature_2 = song2[1]
+    song2_feature_1 = song2[0]
+    song2_feature_2 = song2[1]
+    distance_ = np.trace(np.dot(np.linalg.inv(song2_feature_2), song1_feature_2))
+    diff = song2_feature_1 - song1_feature_1
+    distance_ += np.dot(np.dot((diff).T, np.linalg.inv(song2_feature_2)), diff)
+    distance_ += np.log(np.linalg.det(song2_feature_2) - np.log(np.linalg.det(song1_feature_2)))
+    distance_ -= k
+    return distance_
