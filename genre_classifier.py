6a05118730751779e0e52b02b180ab6b264134ce
David Sohn <dhsohn2@illinois.edu>
2022-10-16 20:59:34 -0500

    added plot_history method

--- genre_classifier.py
+++ genre_classifier.py
@@ -8,6 +8,7 @@ import json
 import numpy as np
 from sklearn.model_selection import train_test_split
 import tensorflow.keras as keras
+import matplotlib.pyplot as plt
 
 DATASET_PATH = './data.json'
 
@@ -20,6 +21,30 @@ def load_data(dataset_path):
     targets = np.array(data['labels'])
 
     return (inputs, targets)
+
+def plot_history(history):
+    fig,axs = plt.subplots(2)
+    
+    #create the acurracy subplot
+    axs[0].plot(history.history["accuracy"], label = "train accuracy")
+    axs[0].plot(history.history["val_accuracy"], label = "test accuracy")
+    axs[0].set_ylabel("accuracy")
+    axs[0].legend(loc="lower right")
+    axs[0].set_title("Accuracy eval")
+
+
+    #create the error subplot
+    axs[1].plot(history.history["loss"], label = "train error")
+    axs[1].plot(history.history["val_loss"], label = "test error")
+    axs[1].set_ylabel("Error")
+    axs[1].set_xlabel("Epoch")
+    axs[1].legend(loc="upper right")
+    axs[1].set_title("Error eval")
+
+    plt.show()
+
+
+
 
 
 if __name__ == '__main__':
@@ -42,3 +67,6 @@ if __name__ == '__main__':
     model.summary()
 
     history = model.fit(inputs_train, targets_train, validation_data=(inputs_test, targets_test), batch_size=32, epochs=50)
+
+    #plot the accuracy and error vs epoch
+    plot_history(history)
