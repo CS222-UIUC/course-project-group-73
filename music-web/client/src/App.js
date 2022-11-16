e70705344411b4075f73cb31ad448183cb006401
Jacob Chang <jacobchang124@gmail.com>
2022-11-16 16:43:31 -0600

    dark mode

--- music-web/client/src/App.js
+++ music-web/client/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import styles from "./styles/App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import pink from "../../flask-server/uploads/PinkPanther30.wav";
 
 function App() {
   const [file, setFile] = useState(null);
   const [audio, setAudio] = useState(null);
   const [genre, setGenre] = useState("");
+  const [theme, setTheme] = useState("light");
 
   function handleAudioPlay() {
     var AudioPlay = new Audio(file);
@@ -59,6 +60,17 @@ function App() {
     }
     return color;
   }
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
 
   // const uploadFile = async (e) => {
   //   e.preventDefault();
@@ -114,135 +126,143 @@ function App() {
   // };
 
   return (
-    <div className={styles.container}>
-      <form className={styles.form}>
-        <h1 className={`${styles.header} text-5xl font-bold`}>
-          Music Genre Classifier
-        </h1>
-        <div class="avatar">
-          <div class="w-24 rounded">
-            <img src="https://www.freeiconspng.com/uploads/light-blue-music-note-picture-15.png" />
+    // <div className={styles.container}>
+    <div className={styles[theme]}>
+      <div className={`${styles.container}`}>
+        <form className={styles.form}>
+          <h1 className={`${styles.header} text-5xl font-bold`}>
+            Music Genre Classifier
+          </h1>
+          <div className={styles.avatar}>
+            <div class="w-24 rounded">
+              <img src="https://www.freeiconspng.com/uploads/light-blue-music-note-picture-15.png" />
+            </div>
           </div>
+        </form>
+        {/* <h1 className="text-5xl font-bold">Music Genre Classifier</h1> */}
+        <div className={styles.utilityBar}>
+          <p>Upload an audio file and see what the genre is.</p>
+          <button className="btn" onClick={toggleTheme}>
+            Switch to {theme == "dark" ? "light" : " dark"} mode
+          </button>
         </div>
-      </form>
-      {/* <h1 className="text-5xl font-bold">Music Genre Classifier</h1> */}
-      <p>Upload an audio file and see what the genre is.</p>
-      {/*
+
+        {/*
       <p>
         Possible outputs = ["blues", "classical", "country", "disco", "hiphop",
         "jazz", "metal", "pop", "reggae", "rock"].
       </p>
       */}
-      <div className={`${styles.warning} alert alert-info shadow-lg`}>
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="stroke-current flex-shrink-0 w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            ></path>
-          </svg>
-          <span>You may only upload .mp3 and .wav files.</span>
-        </div>
-      </div>
-      <div className={styles.badgeWrapper}>
-        <div
-          style={{ backgroundColor: getRandomColor() }}
-          className="badge badge-lg"
-        >
-          blues
-        </div>
-        <div
-          style={{ backgroundColor: getRandomColor() }}
-          className="badge badge-lg"
-        >
-          classical
-        </div>
-        <div
-          style={{ backgroundColor: getRandomColor() }}
-          className="badge badge-lg"
-        >
-          country
-        </div>
-        <div
-          style={{ backgroundColor: getRandomColor() }}
-          className="badge badge-lg"
-        >
-          disco
-        </div>
-        <div
-          style={{ backgroundColor: getRandomColor() }}
-          className="badge badge-lg"
-        >
-          hiphop
-        </div>
-        <div
-          style={{ backgroundColor: getRandomColor() }}
-          className="badge badge-lg"
-        >
-          jazz
-        </div>
-        <div
-          style={{ backgroundColor: getRandomColor() }}
-          className="badge badge-lg"
-        >
-          metal
-        </div>
-        <div
-          style={{ backgroundColor: getRandomColor() }}
-          className="badge badge-lg"
-        >
-          pop
-        </div>
-        <div
-          style={{ backgroundColor: getRandomColor() }}
-          className="badge badge-lg"
-        >
-          reggae
-        </div>
-        <div
-          style={{ backgroundColor: getRandomColor() }}
-          className="badge badge-lg"
-        >
-          rock
-        </div>
-      </div>
-      <form className={styles.form}>
-        <input onChange={uploadFile} type="file" accept=".mp3,.wav"></input>
-
-        <label for="my-modal" class="btn">
-          View Results
-        </label>
-        {/* <button className="btn">View Results</button> */}
-        <input type="checkbox" id="my-modal" className="modal-toggle" />
-        <div className="modal">
-          <div className="modal-box w-11/12 max-w-5xl">
-            <p className="py-4">Genre: "{genre}".</p>
-            <div
-              onClick={handleAudioPlay}
-              className="btn backdrop:btn-sm btn-success"
+        <div className={`${styles.warning} alert alert-info shadow-lg`}>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="stroke-current flex-shrink-0 w-6 h-6"
             >
-              Play Audio
-            </div>
-            <div onClick={handleAudioStop} className="modal-action">
-              <label htmlFor="my-modal" className="btn">
-                Yay!
-              </label>
-            </div>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+            <span>You may only upload .mp3 and .wav files.</span>
           </div>
         </div>
-      </form>
-      {/* <h1>Genre: {genre}</h1> */}
-      {/* <button onClick={handleClick} className="btn">
+        <div className={styles.badgeWrapper}>
+          <div
+            style={{ backgroundColor: getRandomColor() }}
+            className="badge badge-lg"
+          >
+            blues
+          </div>
+          <div
+            style={{ backgroundColor: getRandomColor() }}
+            className="badge badge-lg"
+          >
+            classical
+          </div>
+          <div
+            style={{ backgroundColor: getRandomColor() }}
+            className="badge badge-lg"
+          >
+            country
+          </div>
+          <div
+            style={{ backgroundColor: getRandomColor() }}
+            className="badge badge-lg"
+          >
+            disco
+          </div>
+          <div
+            style={{ backgroundColor: getRandomColor() }}
+            className="badge badge-lg"
+          >
+            hiphop
+          </div>
+          <div
+            style={{ backgroundColor: getRandomColor() }}
+            className="badge badge-lg"
+          >
+            jazz
+          </div>
+          <div
+            style={{ backgroundColor: getRandomColor() }}
+            className="badge badge-lg"
+          >
+            metal
+          </div>
+          <div
+            style={{ backgroundColor: getRandomColor() }}
+            className="badge badge-lg"
+          >
+            pop
+          </div>
+          <div
+            style={{ backgroundColor: getRandomColor() }}
+            className="badge badge-lg"
+          >
+            reggae
+          </div>
+          <div
+            style={{ backgroundColor: getRandomColor() }}
+            className="badge badge-lg"
+          >
+            rock
+          </div>
+        </div>
+        <form className={styles.form}>
+          <input onChange={uploadFile} type="file" accept=".mp3,.wav"></input>
+
+          <label for="my-modal" class="btn">
+            View Results
+          </label>
+          {/* <button className="btn">View Results</button> */}
+          <input type="checkbox" id="my-modal" className="modal-toggle" />
+          <div className="modal">
+            <div className="modal-box w-11/12 max-w-5xl">
+              <p className="py-4">Genre: "{genre}".</p>
+              <div
+                onClick={handleAudioPlay}
+                className="btn backdrop:btn-sm btn-success"
+              >
+                Play Audio
+              </div>
+              <div onClick={handleAudioStop} className="modal-action">
+                <label htmlFor="my-modal" className="btn">
+                  exit
+                </label>
+              </div>
+            </div>
+          </div>
+        </form>
+        {/* <h1>Genre: {genre}</h1> */}
+        {/* <button onClick={handleClick} className="btn">
         Upload
       </button> */}
-      {/* <br />
+        {/* <br />
       <br />
       <br />
       <br />
@@ -250,7 +270,7 @@ function App() {
       <br />
       <br />
       <br /> */}
-      {/* <div class="avatar-group -space-x-6">
+        {/* <div class="avatar-group -space-x-6">
         <div class="avatar">
           <div class="w-12">
             <img src="https://canvas.illinois.edu/images/thumbnails/5557236/TSSHozO2UuzaG5XQqBHQoZBgmc2TDaSex4RvYGjD" />
@@ -272,46 +292,49 @@ function App() {
           </div>
         </div>
       </div> */}
-      <footer class="footer footer-center items-center p-4 bg-neutral text-neutral-content">
-        <div class="avatar-group -space-x-6">
-          <div class="avatar">
-            <div class="w-12">
-              <img src="https://canvas.illinois.edu/images/thumbnails/5557236/TSSHozO2UuzaG5XQqBHQoZBgmc2TDaSex4RvYGjD" />
-            </div>
-          </div>
-          <div class="avatar">
-            <div class="w-12">
-              <img src="https://canvas.illinois.edu/images/thumbnails/5725397/DplRsnCFyu8ucKAnv6ngMZbCILvzX6xT74HjNh1M" />
-            </div>
-          </div>
-          <div class="avatar">
-            <div class="w-12">
-              <img src="https://canvas.illinois.edu/images/thumbnails/5625870/vM7bPiajUwVZhlLp7fS88wJEM5XAZ2uaWsl26tIB" />
-            </div>
-          </div>
-          <div class="avatar">
-            <div class="w-12">
-              <img src="https://canvas.illinois.edu/images/thumbnails/5738960/Cb35NY02lYzDF5x1g7Jg8sQfqKWVzuiMwkDBZgnN" />
-            </div>
-          </div>
-        </div>
-        <div class="items-center grid-flow-col">
-          <svg
-            width="36"
-            height="36"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            class="fill-current"
+        <footer class="footer footer-center items-center p-4 bg-neutral text-neutral-content">
+          <div
+            style={{ marginTop: "1rem", marginBottom: "0rem" }}
+            class="avatar-group -space-x-6"
           >
-            <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-          </svg>
-          <p>
-            Developed by: Jacob Chang, Max Lindsey, Daniel Murphy, David Sohn
-          </p>
-        </div>
-        {/* <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+            <div class="avatar">
+              <div class="w-12">
+                <img src="https://canvas.illinois.edu/images/thumbnails/5557236/TSSHozO2UuzaG5XQqBHQoZBgmc2TDaSex4RvYGjD" />
+              </div>
+            </div>
+            <div class="avatar">
+              <div class="w-12">
+                <img src="https://canvas.illinois.edu/images/thumbnails/5725397/DplRsnCFyu8ucKAnv6ngMZbCILvzX6xT74HjNh1M" />
+              </div>
+            </div>
+            <div class="avatar">
+              <div class="w-12">
+                <img src="https://canvas.illinois.edu/images/thumbnails/5625870/vM7bPiajUwVZhlLp7fS88wJEM5XAZ2uaWsl26tIB" />
+              </div>
+            </div>
+            <div class="avatar">
+              <div class="w-12">
+                <img src="https://canvas.illinois.edu/images/thumbnails/5738960/Cb35NY02lYzDF5x1g7Jg8sQfqKWVzuiMwkDBZgnN" />
+              </div>
+            </div>
+          </div>
+          <div class="items-center grid-flow-col">
+            <svg
+              width="36"
+              height="36"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              class="fill-current"
+            >
+              <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
+            </svg>
+            <p>
+              Developed by: Jacob Chang, Max Lindsey, Daniel Murphy, David Sohn
+            </p>
+          </div>
+          {/* <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
           <a>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -346,7 +369,8 @@ function App() {
             </svg>
           </a>
         </div> */}
-      </footer>
+        </footer>
+      </div>
     </div>
   );
 }
